a50b8a1b759ac37dc6dd73e9791c53cfc8917062
Ade Cuellar <76180286+adecuellarv@users.noreply.github.com>
2026-02-12 22:30:30 -0600

    update version node y test para inquirer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hola linkOn");
+import inquirer from 'inquirer';
+
+async function main() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'nombre',
+      message: '¿Cuál es tu nombre?',
+    },
+    {
+      type: 'list',
+      name: 'lenguaje',
+      message: '¿Qué lenguaje prefieres?',
+      choices: ['JavaScript', 'Python', 'C++', 'Go'],
+    },
+  ]);
+
+  console.log('Respuestas:', answers);
+}
+
+main();
